21f1db76e4d4340b97edd7e3cea7e1d88e03e44d
gimseondong <dong@donglab.com>
2017-09-28 00:00:03 +0900

    Ver 0.1
    Signed-off-by: gimseondong <dong@donglab.com>

---
+++ src/com/donglab/dong/Trocar.java
@@ -0,0 +1,13 @@
+package com.donglab.dong;
+
+import java.sql.Connection;
+
+public class Trocar {
+
+    Connection conn = null;
+
+
+    String url = "jdbc:mariadb://www.kellogzz.com:3306/chapp";
+    String id = "appch";
+    String pwd = "angel12,./dong";
+}
